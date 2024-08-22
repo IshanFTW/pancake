d75899c7be19250e43430ca42c2299b0b8c90782
IshanFTW <ishanjindal2003@gmail.com>
2024-08-22 15:46:12 +0530

    testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import rootRouter from './routes/index'
 import cors from 'cors';
+const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/v1', rootRouter);
 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
